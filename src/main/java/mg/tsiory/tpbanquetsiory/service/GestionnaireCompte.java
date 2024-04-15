062f1257dbcba72e1ef47c6f450f44fadaeab7bc
tsioryrovantsoa <tsioryrovantsoa@gmail.com>
2024-04-15 14:53:18 +0300

    Déclaration de la source de données

---
+++ src/main/java/mg/tsiory/tpbanquetsiory/service/GestionnaireCompte.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.tsiory.tpbanquetsiory.service;
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.RequestScoped;
+/**
+ *
+ * @author tsiory
+ */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",              // nom et
+    password="toor", // mot de passe que vous avez donnés lors de la création de la base de données
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true",
+      "driverClass=com.mysql.cj.jdbc.Driver"
+    }
+)
+@RequestScoped
+public class GestionnaireCompte {
+    
+}
