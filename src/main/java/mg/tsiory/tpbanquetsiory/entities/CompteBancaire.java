a43e6b8b327f7642713ded0dad0e1b602e98de09
tsioryrovantsoa <tsioryrovantsoa@gmail.com>
2024-04-17 15:44:28 +0300

    entité OperationBancaire

--- src/main/java/mg/tsiory/tpbanquetsiory/entities/CompteBancaire.java
+++ src/main/java/mg/tsiory/tpbanquetsiory/entities/CompteBancaire.java
@@ -4,14 +4,19 @@
  */
 package mg.tsiory.tpbanquetsiory.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -97,6 +102,13 @@ public class CompteBancaire implements Serializable {
     @Override
     public String toString() {
         return "mg.tsiory.tpbanquetsiory.entities.CompteBancaire[ id=" + id + " ]";
+    }
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<OperationBancaire> operations = new ArrayList<>();
+
+    public List<OperationBancaire> getOperations() {
+        return operations;
     }
 
 }
---
+++ src/main/java/mg/tsiory/tpbanquetsiory/entities/OperationBancaire.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.tsiory.tpbanquetsiory.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author tsiory
+ */
+@Entity
+public class OperationBancaire {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private LocalDateTime dateOperation;
+    private int montant;
+
+    public OperationBancaire() {
+    }
+
+    public OperationBancaire(String description, int montant) {
+        this.description = description;
+        this.montant = montant;
+        dateOperation = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDateOperation() {
+        return dateOperation;
+    }
+
+    public void setDateOperation(LocalDateTime dateOperation) {
+        this.dateOperation = dateOperation;
+    }
+
+    public int getMontant() {
+        return montant;
+    }
+
+    public void setMontant(int montant) {
+        this.montant = montant;
+    }
+
+}
